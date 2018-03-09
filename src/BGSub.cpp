2864ab3cb929913e538bb15e8110586128786f00
Veerachart Srisamosorn <veerachart.sr@gmail.com>
2018-03-09 16:32:55 +0900

    Changed back to 2D KF only. The size is kept constant, and updated by the new detection only. Added a VideoWriter to write the output to an avi file.

--- src/BGSub.cpp
+++ src/BGSub.cpp
@@ -82,6 +82,10 @@ private:
 	float sdBody;
 	float sdHead;
 
+	// To keep body and head widths (constanted, changed only by new detections
+	float bodyWidth;
+	float headWidth;
+
 	// For estimating head position & size based on human detection
 	float heightRatio;		// head's height relative to window's height
 	float deltaAngle;
@@ -94,31 +98,28 @@ private:
 };
 
 TrackedObjects::TrackedObjects(RotatedRect objDetection, bool isHumanDetected, bool isHeadDetected, RotatedRect headDetection) {
-	objectKF = KalmanFilter(6, 3, 0);
+	objectKF = KalmanFilter(4, 2, 0);
 	//headKF = KalmanFilter(3, 3, 3);
-	headKF = KalmanFilter(6, 3, 0);
+	headKF = KalmanFilter(4, 2, 0);
 	sdBody = objDetection.size.width/4.;
 	sdHead = objDetection.size.width/4.;
 
-	objectKF.transitionMatrix = (Mat_<float>(6,6) << 1,0,0,1,0,0,
-													 0,1,0,0,1,0,
-													 0,0,1,0,0,1,
-													 0,0,0,1,0,0,
-													 0,0,0,0,1,0,
-													 0,0,0,0,0,1);
+	objectKF.transitionMatrix = (Mat_<float>(4,4) << 1,0,1,0,
+													 0,1,0,1,
+													 0,0,1,0,
+													 0,0,0,1);
 	setIdentity(objectKF.measurementMatrix);
 	setIdentity(objectKF.processNoiseCov, Scalar::all(16.0));
 	setIdentity(objectKF.measurementNoiseCov, Scalar::all(sdBody*sdBody));
 	setIdentity(objectKF.errorCovPost, Scalar::all(sdBody*sdBody));
-	objectKF.statePost = (Mat_<float>(6,1) << objDetection.center.x, objDetection.center.y, objDetection.size.width, 0, 0, 0);
+	objectKF.statePost = (Mat_<float>(4,1) << objDetection.center.x, objDetection.center.y, 0, 0);
+	bodyWidth = objDetection.size.width;
 	objectROI = objDetection;
 
-	headKF.transitionMatrix = (Mat_<float>(6,6) << 1,0,0,1,0,0,
-												   0,1,0,0,1,0,
-												   0,0,1,0,0,1,
-												   0,0,0,1,0,0,
-												   0,0,0,0,1,0,
-												   0,0,0,0,0,1);
+	headKF.transitionMatrix = (Mat_<float>(4,4) << 1,0,1,0,
+												   0,1,0,1,
+												   0,0,1,0,
+												   0,0,0,1);
 	setIdentity(headKF.measurementMatrix);
 	setIdentity(headKF.processNoiseCov, Scalar::all(16.0));
 	setIdentity(headKF.measurementNoiseCov, Scalar::all(sdHead*sdHead));
@@ -131,7 +132,8 @@ TrackedObjects::TrackedObjects(RotatedRect objDetection, bool isHumanDetected, b
 		heightRatio = norm(headDetection.center - objDetection.center)/ objDetection.size.height;
 		deltaAngle = headDetection.angle - objDetection.angle;
 		headRatio = headDetection.size.width/objDetection.size.width;
-		headKF.statePost = (Mat_<float>(6,1) << headDetection.center.x, headDetection.center.y,headDetection.size.width, 0, 0, 0);
+		headKF.statePost = (Mat_<float>(4,1) << headDetection.center.x, headDetection.center.y, 0, 0);
+		headWidth = headDetection.size.width;
 		headROI = headDetection;
 	}
 	else {
@@ -140,8 +142,9 @@ TrackedObjects::TrackedObjects(RotatedRect objDetection, bool isHumanDetected, b
 		headRatio = 0.375;
 		float theta_r = (objDetection.angle + deltaAngle)*CV_PI/180.;
 		Point2f headCenter = objDetection.center + heightRatio*objDetection.size.height*Point2f(sin(theta_r), -cos(theta_r));
-		headKF.statePost = (Mat_<float>(6,1) << headCenter.x, headCenter.y, headRatio*objDetection.size.width, 0, 0, 0);
-		headROI = RotatedRect(headCenter, Size(headRatio*objDetection.size.width, headRatio*objDetection.size.width), objDetection.angle);
+		headKF.statePost = (Mat_<float>(4,1) << headCenter.x, headCenter.y, 0, 0);
+		headWidth = headRatio*objDetection.size.width;
+		headROI = RotatedRect(headCenter, Size(headWidth, headWidth), objDetection.angle);
 	}
 	//setIdentity(headKF.transitionMatrix);
 	//setIdentity(headKF.controlMatrix);
@@ -159,13 +162,13 @@ Point2f TrackedObjects::PredictObject() {
 	Mat prediction = objectKF.predict();
 	sdBody = sqrt(min(objectKF.errorCovPost.at<float>(0,0), objectKF.errorCovPost.at<float>(1,1)));
 	objectROI = RotatedRect(Point2f(prediction.at<float>(0,0), prediction.at<float>(1,0)),
-							Size2f(prediction.at<float>(2,0), 2*prediction.at<float>(2,0)),
+							Size2f(bodyWidth, 2*bodyWidth),
 							atan2(prediction.at<float>(0,0) - img_center.x, img_center.y - prediction.at<float>(1,0)) *180./CV_PI);
 
 	Mat predictHead = headKF.predict();
 	sdHead = sqrt(min(headKF.errorCovPost.at<float>(0,0), headKF.errorCovPost.at<float>(1,1)));
 	headROI = RotatedRect(Point2f(predictHead.at<float>(0,0), predictHead.at<float>(1,0)),
-						  Size2f(predictHead.at<float>(2,0), predictHead.at<float>(2,0)),
+						  Size2f(headWidth, headWidth),
 						  atan2(predictHead.at<float>(0,0) - img_center.x, img_center.y - predictHead.at<float>(1,0)) *180./CV_PI);
 
 	return Point2f(prediction.at<float>(0,0), prediction.at<float>(1,0));
@@ -206,25 +209,28 @@ Point2f TrackedObjects::UpdateObject(RotatedRect objDetection, bool isHumanDetec
 				status = HUMAN;
 		}
 		setIdentity(objectKF.measurementNoiseCov, Scalar::all(objDetection.size.width*objDetection.size.width/16.));
-		measurement = (Mat_<float>(3,1) << objDetection.center.x, objDetection.center.y, objDetection.size.width);
+		measurement = (Mat_<float>(2,1) << objDetection.center.x, objDetection.center.y);
+		bodyWidth = objDetection.size.width;
 	}
 	else {
 		setIdentity(objectKF.measurementNoiseCov, Scalar::all(objDetection.size.width*objDetection.size.width/16.));		// Larger variance for object
-		measurement = (Mat_<float>(3,1) << objDetection.center.x, objDetection.center.y, objectROI.size.width);
+		measurement = (Mat_<float>(2,1) << objDetection.center.x, objDetection.center.y);
+		bodyWidth = objectROI.size.width;
 	}
 	Mat corrected_state = objectKF.correct(measurement);
 	objectROI = RotatedRect(Point2f(corrected_state.at<float>(0,0), corrected_state.at<float>(1,0)),
-							Size2f(corrected_state.at<float>(2,0), 2*corrected_state.at<float>(2,0)),
+							Size2f(bodyWidth, 2*bodyWidth),
 							atan2(corrected_state.at<float>(0,0) - img_center.x, img_center.y - corrected_state.at<float>(1,0)) *180./CV_PI);
 
 	// After updating the object, use it as weak measurement of head
 	float theta_r = (objectROI.angle + deltaAngle)*CV_PI/180.;
 	Point2f headCenter = objectROI.center + heightRatio*objectROI.size.height*Point2f(sin(theta_r), -cos(theta_r));
-	Mat measurement_frombody = (Mat_<float>(3,1) << headCenter.x, headCenter.y, headRatio*objectROI.size.width);
+	Mat measurement_frombody = (Mat_<float>(2,1) << headCenter.x, headCenter.y);
+	headWidth = headRatio*objectROI.size.width;
 	setIdentity(headKF.measurementNoiseCov, Scalar::all(objectROI.size.width*objectROI.size.width/16.));
 	Mat corrected_head = headKF.correct(measurement_frombody);
 	headROI = RotatedRect(Point2f(corrected_head.at<float>(0,0), corrected_head.at<float>(1,0)),
-						  Size(corrected_head.at<float>(2,0), corrected_head.at<float>(2,0)),
+						  Size(headWidth, headWidth),
 						  atan2(corrected_head.at<float>(0,0) - img_center.x, img_center.y - corrected_head.at<float>(1,0)) *180./CV_PI);
 	//heightRatio = 0.3125;
 	//deltaAngle = 0.;
@@ -246,12 +252,13 @@ Point2f TrackedObjects::UpdateObject(RotatedRect objDetection, bool isHumanDetec
 }*/
 
 Point2f TrackedObjects::UpdateHead(RotatedRect headDetection) {
-	Mat measurement = (Mat_<float>(3,1) << headDetection.center.x, headDetection.center.y, headDetection.size.width);
+	Mat measurement = (Mat_<float>(2,1) << headDetection.center.x, headDetection.center.y);
+	headWidth = headDetection.size.width;
 	setIdentity(headKF.measurementNoiseCov, Scalar::all(headDetection.size.width*headDetection.size.width/16.));
 	Mat corrected_state = headKF.correct(measurement);
 	sdHead = sqrt(min(headKF.errorCovPost.at<float>(0,0), headKF.errorCovPost.at<float>(1,1)));
 	headROI = RotatedRect(Point2f(corrected_state.at<float>(0,0), corrected_state.at<float>(1,0)),
-						  Size2f(corrected_state.at<float>(2,0), corrected_state.at<float>(2,0)),
+						  Size2f(headWidth, headWidth),
 						  atan2(corrected_state.at<float>(0,0) - img_center.x, img_center.y - corrected_state.at<float>(1,0)) *180./CV_PI);
 	heightRatio = norm(headROI.center - objectROI.center) / objectROI.size.height;
 	deltaAngle = headROI.angle - objectROI.angle;
@@ -317,7 +324,7 @@ int TrackedObjects::getStatus() {
 }
 
 Point2f TrackedObjects::getHeadVel() {
-	return Point2f(headKF.statePost.at<float>(3,0), headKF.statePost.at<float>(4,0));
+	return Point2f(headKF.statePost.at<float>(2,0), headKF.statePost.at<float>(3,0));
 }
 
 int TrackedObjects::getDirection() {
@@ -332,10 +339,10 @@ void TrackedObjects::updateDirection(int estimation, int movingDirection) {
 
 	int diff = abs(estimation - headDirection);
 	if (diff <= 45) {				// <= 45 degree change
-		headDirection = (headDirection + estimation) /2;
+		headDirection = cvRound((headDirection + estimation)/2.);
 	}
 	else if ( diff >= 315) {		// <= 45 degree change, crossing the line 0,360
-		headDirection = (headDirection + estimation + 360) /2;
+		headDirection = cvRound((headDirection + estimation + 360)/2.);
 		if (headDirection > 360)
 			headDirection -= 360;
 	}
@@ -345,12 +352,12 @@ void TrackedObjects::updateDirection(int estimation, int movingDirection) {
 			// Moving
 			if (abs(movingDirection - headDirection) >= 180) {
 				// crossing 0,360 line
-				headDirection = (headDirection + movingDirection + 360) /2;
+				headDirection = cvRound((headDirection + movingDirection + 360)/2.);
 				if (headDirection > 360)
 					headDirection -= 360;
 			}
 			else {
-				headDirection = (headDirection + movingDirection) /2;
+				headDirection = cvRound((headDirection + movingDirection)/2.);
 			}
 		}
 	}
@@ -404,7 +411,7 @@ class BGSub {
     
     int dilation_size;
     
-    //VideoWriter outputVideo;
+    VideoWriter outputVideo;
     //bool save_video;
     
     FisheyeHOGDescriptor hog_body;
@@ -477,6 +484,13 @@ class BGSub {
                     return;
                 }
             }*/
+
+            string videoName = "omni1A_test12.avi";
+            outputVideo.open(videoName, CV_FOURCC('D','I','V','X'), 10, Size(800, 600), true);
+			if (!outputVideo.isOpened()) {
+				cerr << "Could not write video." << endl;
+				return;
+			}
             pMOG2 = BackgroundSubtractorMOG2(1000, 16, true);
             pMOG2.set("backgroundRatio", 0.8);
             toDraw = _toDraw;
@@ -798,6 +812,7 @@ class BGSub {
 								walking_dir += 360;					// [0, 360) range. Negative means no clue
 						}
 						//cout << head_vel << " Moving in " << walking_dir << " degree direction" << endl;
+						cout << rect.center << " " << rect.size << " " << rect.angle << endl;
 						// crop head area
 						Mat M = getRotationMatrix2D(rect.center, rect.angle, 1.0);
 						Mat rotated, cropped;
@@ -1023,6 +1038,8 @@ class BGSub {
 
 					imshow("FG Mask MOG 2", fgMaskMOG2);
 					imshow("Detection", input_img);
+
+					outputVideo << input_img;
 				}
 				//waitKey(1);
 				//std::cout << end-begin << std::endl;
@@ -1140,7 +1157,7 @@ int main (int argc, char **argv) {
     {
         frame = imread(path_dir+file_list[idx]);
         //int64 start = getTickCount();
-        if( frame.empty() )
+        if( frame.data == NULL )
             break;
             
         int64 start = getTickCount();
@@ -1149,12 +1166,13 @@ int main (int argc, char **argv) {
         cout << double(time)/getTickFrequency() << endl;
         
         if (toDraw) {
-            char c = waitKey(0);
+            char c = waitKey(1);
             if (c == 27)
                 break;
         }
         idx++;
     }
+    cout << "Finished" << endl;
     return 0;
 }
 
